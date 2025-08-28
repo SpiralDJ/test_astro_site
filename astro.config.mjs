b303af8ef97b7f1323a3b3acb612dc888a1e7254
SpiralDJ <pencilsarelikepencils@gmail.com>
2025-08-28 05:54:11 -0400

    Set site and base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://spiraldj.github.io/test_astro_site',
+  base: '/test_astro_site',
+});
