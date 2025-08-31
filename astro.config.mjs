896a6cc810f4d98c180d2468830423933f97df71
SpiralDJ <pencilsarelikepencils@gmail.com>
2025-08-31 19:00:21 -0400

    Added i framer and mdx to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://spiraldj.github.io/test_astro_site',
   base: '/test_astro_site',
+  integrations: [mdx()],
 });
